4c502fc9ed9d9e18de5ad2d112a0a07a6241f616
zumabektairbekuulu53 <zumabektairbekuulu53@gmail.com>
2025-06-27 16:14:33 -0500

    Delete New folder directory

--- New folder/pages/index.jsx
+++
@@ -1,6 +0,0 @@
-import dynamic from "next/dynamic";
-const ExcelUploadTable = dynamic(() => import("../components/ExcelUploadTable"), { ssr: false });
-
-export default function Home() {
-  return <ExcelUploadTable />;
-}
