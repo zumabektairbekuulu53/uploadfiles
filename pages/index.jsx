9371b7c0ba160ce93b389035c36426bd5fb93cbd
zumabektairbekuulu53 <zumabektairbekuulu53@gmail.com>
2025-06-27 21:05:00 -0500

    Add files via upload

---
+++ pages/index.jsx
@@ -0,0 +1,7 @@
+
+import dynamic from "next/dynamic";
+const ExcelUploadTable = dynamic(() => import("../components/ExcelUploadTable"), { ssr: false });
+
+export default function Home() {
+  return <ExcelUploadTable />;
+}
